fe805cc7d137304bcb37233e6cd304d014c3a632
Levi Janke <levijanke@levis-mbp.lan>
2024-07-22 23:33:28 -0600

    transfer grapple WIP to main pc

--- Script/Grapple.gd
+++ Script/Grapple.gd
@@ -2,19 +2,21 @@ extends RigidBody2D
 
 
 @export var radius: int
-@export var distance: int
-@export var strength = 50
+@export var distance: RayCast2D
+@export var strength = 40000
 
 var thrown = false
+var retracted = true
 var player
 
 
 func _ready():
 	player = $"../Player"
+	#distance.set_collision_mask(16)
 
 func _process(delta):
 	
-	if !thrown:
+	if !thrown && retracted:
 		self.position = player.position
 		
 		if Input.is_action_just_pressed("grapple"):
@@ -22,10 +24,15 @@ func _process(delta):
 			var direction = get_global_mouse_position() - transform.origin
 			var impulse_direction = direction.normalized()
 			throw_grapple(strength, impulse_direction)
-			print(direction)
-	else:
+			print(impulse_direction)
+	elif thrown && !retracted:
+		#distance.target_position = player.position
+		if self.position.distance_to(player.position) > 50:
+			self.linear_velocity = Vector2.ZERO
 		if Input.is_key_pressed(KEY_R):
-			thrown = false	
+			thrown = false
+			retracted = true
+			self.position = player.position
 	
 
 
@@ -35,4 +42,4 @@ func _physics_process(delta):
 	
 func throw_grapple(throw_strength, direction):
 	thrown = true
-	self.apply_central_force(direction)
+	self.apply_central_force(direction * throw_strength)
