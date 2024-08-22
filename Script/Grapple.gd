c9a198baafd4631aa46d6d94f455e5bb5bc48143
loaphs <loaphs@gmail.com>
2024-08-22 15:54:55 -0600

    Working on grapple, its wack

--- Script/Grapple.gd
+++ Script/Grapple.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 
 @export var radius: int
 @export var distance: RayCast2D
-@export var strength = 40000
+@export var strength = 750
 
 var thrown = false
 var retracted = true
@@ -16,30 +16,28 @@ func _ready():
 
 func _process(delta):
 	
-	if !thrown && retracted:
+	print(position)
+	if !thrown:
 		self.position = player.position
 		
 		if Input.is_action_just_pressed("grapple"):
-			
 			var direction = get_global_mouse_position() - transform.origin
 			var impulse_direction = direction.normalized()
 			throw_grapple(strength, impulse_direction)
 			print(impulse_direction)
-	elif thrown && !retracted:
+	elif thrown:
 		#distance.target_position = player.position
-		if self.position.distance_to(player.position) > 50:
+		if self.position.distance_to(player.position) > 500:
 			self.linear_velocity = Vector2.ZERO
 		if Input.is_key_pressed(KEY_R):
 			thrown = false
-			retracted = true
-			self.position = player.position
 	
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	pass
 	
 	
 func throw_grapple(throw_strength, direction):
 	thrown = true
-	self.apply_central_force(direction * throw_strength)
+	self.apply_central_impulse(direction * throw_strength)
